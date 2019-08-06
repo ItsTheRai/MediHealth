f90a4fdcde66b75e658bdf294879a762789ebc91
Rai <rmndss@hotmail.com>
2019-08-06 21:59:09 +0100

    correct class name

--- src/test/java/com/medihealth/billing/PriceCalculatorTest.java
+++ src/test/java/com/medihealth/billing/PriceCalculatorTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class priceCalculatorTest {
+public class PriceCalculatorTest {
 
     @Test
     public void treatmentCost() {
