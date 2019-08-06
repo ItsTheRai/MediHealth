d50dece2a4f6862ba9630ba603eb6ebbff0a6370
Rai <rmndss@hotmail.com>
2019-08-06 21:03:07 +0100

    add senior discounting

--- src/main/java/com/medihealth/billing/discounting/BasicDiscountingService.java
+++ src/main/java/com/medihealth/billing/discounting/BasicDiscountingService.java
@@ -1,13 +1,19 @@
 package com.medihealth.billing.discounting;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BasicDiscountingService implements DiscountingService {
 
-    private final static IntStream child_range = IntStream.rangeClosed(0, 5);
+    private final static List<Integer> CHILD_RANGE = IntStream.rangeClosed(0, 5).boxed().collect(Collectors.toList());
+    private final static List<Integer> SENIOR_1_RANGE = IntStream.rangeClosed(65, 70).boxed().collect(Collectors.toList());
+    private final static List<Integer> SENIOR_2_RANGE = IntStream.rangeClosed(90, 125).boxed().collect(Collectors.toList());
 
     private final static BigDecimal CHILD_DISCOUNT = new BigDecimal("0.40");
+    private final static BigDecimal SENIOR_1_DISCOUNT = new BigDecimal("0.60");
+    private final static BigDecimal SENIOR_2_DISCOUNT = new BigDecimal("0.90");
 
     @Override
     public int getAgeBasedDiscount(int age, int cost) {
@@ -15,8 +21,12 @@ public class BasicDiscountingService implements DiscountingService {
             throw new IllegalArgumentException();
         }
 
-        if (child_range.anyMatch(value -> value == age)) {
+        if (CHILD_RANGE.contains(age)) {
             return CHILD_DISCOUNT.multiply(BigDecimal.valueOf(cost)).intValue();
+        } else if (SENIOR_1_RANGE.contains(age)) {
+            return SENIOR_1_DISCOUNT.multiply(BigDecimal.valueOf(cost)).intValue();
+        } else if (SENIOR_2_RANGE.contains(age)) {
+            return SENIOR_2_DISCOUNT.multiply(BigDecimal.valueOf(cost)).intValue();
         }
         throw new IllegalArgumentException();
     }
--- src/test/java/com/medihealth/billing/discounting/DiscountingServiceTest.java
+++ src/test/java/com/medihealth/billing/discounting/DiscountingServiceTest.java
@@ -6,6 +6,12 @@ import static org.junit.Assert.assertEquals;
 
 public class DiscountingServiceTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testAgeDiscountFailure() {
+        DiscountingService discountingService = new BasicDiscountingService();
+        discountingService.getAgeBasedDiscount(-9, 100);
+    }
+
     @Test
     public void testChildDiscount() {
         DiscountingService discountingService = new BasicDiscountingService();
@@ -13,9 +19,17 @@ public class DiscountingServiceTest {
         assertEquals(40, ageBasedDiscount);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testAgeDiscountFailure() {
+    @Test
+    public void testSenior1Discount() {
         DiscountingService discountingService = new BasicDiscountingService();
-        discountingService.getAgeBasedDiscount(-9, 100);
+        int ageBasedDiscount = discountingService.getAgeBasedDiscount(67, 100);
+        assertEquals(60, ageBasedDiscount);
+    }
+
+    @Test
+    public void testSenior2Discount() {
+        DiscountingService discountingService = new BasicDiscountingService();
+        int ageBasedDiscount = discountingService.getAgeBasedDiscount(99, 100);
+        assertEquals(90, ageBasedDiscount);
     }
 }
